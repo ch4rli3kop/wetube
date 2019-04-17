6b257099121b0540ccec6c9d35e668449a26d7cb
ch4rli3kop <pch2180@gmail.com>
2019-04-18 07:02:41 +0900

    add pug

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -5,10 +5,10 @@ import { videos, videoDetail, editVideo, upload, deleteVideo } from "../controll
 const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 
 export default videoRouter;
