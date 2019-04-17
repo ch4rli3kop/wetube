be9e900e21b672aebcf7416cc39dbed4066e5420
ch4rli3kop <pch2180@gmail.com>
2019-04-17 22:54:21 +0900

    mvc 구조로 정리함

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,10 @@
+// global
 export const join = (req, res) => res.send("Join");
 export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Logout");
+
+// users
+export const users = (req, res) => res.send("Users");
+export const editProfile = (req, res) => res.send("Edit Profile");
+export const changePassword = (req, res) => res.send("Change Password");
+export const userDetail = (req, res) => res.send("User Detail");
