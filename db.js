1f2d4dc8de554f63c3eb46e8d3e18f023fae3f99
ch4rli3kop <pch2180@gmail.com>
2019-04-26 17:28:23 +0900

    install mongodb and mongoose, dotenv

--- db.js
+++ db.js
@@ -1,50 +1,16 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube",
     {
-        id: 123123,
-        title: 'Test Video1',
-        description: "fucking hack video",
-        views: 24,
-        videoFile: "http://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.mp4",
-        creator: {
-            id: 15365,
-            name: "charlie",
-            email: "pch2180@naver.com"
-        }
-    },
-    {
-        id: 11231231,
-        title: 'Test Video2',
-        description: "fucking crazy video",
-        views: 24,
-        videoFile: "http://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.mp4",
-        creator: {
-            id: 15365,
-            name: "charlie",
-            email: "pch2180@naver.com"
-        }
-    },
-    {
-        id: 11123,
-        title: 'Test Video3',
-        description: "fucking coool video",
-        views: 24,
-        videoFile: "http://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.mp4",
-        creator: {
-            id: 15365,
-            name: "charlie",
-            email: "pch2180@naver.com"
-        }
-    },
-    {
-        id: 23423,
-        title: 'Test Video4',
-        description: "fucking awesome video",
-        views: 24,
-        videoFile: "http://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.mp4",
-        creator: {
-            id: 15365,
-            name: "charlie",
-            email: "pch2180@naver.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log(`Error on DB Connection: ${error}`)
+
+db.once("open", handleOpen);
+db.on("error", handleError);
