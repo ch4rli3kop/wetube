71ae31ae3ee639b2e2b4b3163ff34dd91a8254bc
ch4rli3kop <pch2180@gmail.com>
2019-04-26 11:35:22 +0900

    finish controller before process db

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import {videos} from "../db";
+import routes from "../routes";
 // global
 export const home = (req, res) => {
     res.render("home", { pageTitle: "Home", videos});
@@ -6,7 +7,7 @@ export const home = (req, res) => {
 
 export const search = (req, res) => {
     const {query: {searchingBy} } = req; // const searchingBy = req.query.term;
-    res.render("search", { pageTitle: "Search", searchingBy });
+    res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 
@@ -14,6 +15,14 @@ export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "
 
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
 
-export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+export const getUpload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description }
+    } = req;
+    // To Do: Upload and save Video
+    res.redirect(routes.videoDetail(123123));
+};
 
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,20 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import { postJoin, getJoin, getlogin, postlogin, logout } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getlogin);
+globalRouter.post(routes.login, postlogin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
+
+
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
